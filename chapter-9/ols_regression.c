cc8620f3eb2d1cec6f8e9f2139ecd31b9cf1b1e7
Jodie Burchell <jodie.burchell@gmail.com>
2026-01-29 08:57:25 +0100

    Refactored OLS regression implementation.

--- chapter-9/ols_regression.c
+++ chapter-9/ols_regression.c
@@ -5,110 +5,75 @@
 
 #include <stdio.h>
 
-# define N_COLS 2
+#define N_ROWS 3
+#define N_COLS 2
+
+void transpose_matrix(int m, int n, float original_matrix[m][n], float transposed_matrix[n][m]) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            transposed_matrix[j][i] = original_matrix[i][j];
+        }
+    }
+}
+
+void multiply_matrices(int m, int n, int p, float matrix_1[m][n], float matrix_2[n][p], float output_matrix[m][p]) {
+    float dot_product;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            dot_product = 0.0f;
+            for (int k = 0; k < n; k++) {
+                dot_product += matrix_1[i][k] * matrix_2[k][j];
+            }
+            output_matrix[i][j] = dot_product;
+        }
+    }
+}
 
 int main(void) {
-    int N_ROWS;
-
-    printf("Number of observations: ");
-    scanf("%d", &N_ROWS);
-
-    float X[N_ROWS][N_COLS], y[N_ROWS], X_T[N_COLS][N_ROWS], X_T_X[N_COLS][N_COLS], X_T_y[N_COLS], y_hat[N_ROWS];
-    float u_11, u_12, l_21, u_22, z_0, z_1, b_0, b_1, sum;
-
-    printf("Enter the feature values: ");
-    for (int i = 0; i < N_ROWS; i++) {
-        scanf("%f", &X[i][1]);
-        X[i][0] = 1.0f;
-    }
-
-    printf("Enter the target values: ");
-    for (int i = 0; i < N_ROWS; i++) {
-        scanf("%f", &y[i]);
-    }
+    float X[N_ROWS][N_COLS] = {{1, 1,}, {1, 2}, {1, 3}}, y[N_ROWS][1] = {3, 6, 7};
+    float X_T[N_COLS][N_ROWS], X_T_X[N_COLS][N_COLS], X_T_y[N_COLS][1], y_hat[N_ROWS][1];
+    float u_11, u_12, l_21, u_22, z_0, z_1, b_0, b_1;
 
     // Transpose the matrix
-    for (int i = 0; i < N_ROWS; i++) {
-        for (int j = 0; j < N_COLS; j++) {
-            X_T[j][i] = X[i][j];
-        }
-    }
+    transpose_matrix(N_ROWS, N_COLS, X, X_T);
 
     // Multiply the matrices to get X_T_X
-    for (int i = 0; i < N_COLS; i++) {
-        for (int j = 0; j < N_COLS; j++) {
-            sum = 0.0f;
-            for (int k = 0; k < N_ROWS; k++) {
-                sum += X_T[i][k] * X[k][j];
-            }
-            X_T_X[i][j] = sum;
-        }
-    }
+    multiply_matrices(N_COLS, N_ROWS, N_COLS, X_T, X, X_T_X);
 
     // Multiply the matrices to get X_T_y
-    for (int i = 0; i < N_COLS; i++) {
-        for (int j = 0; j < 1; j++) {
-            sum = 0.0f;
-            for (int k = 0; k < N_ROWS; k++) {
-                sum += X_T[i][k] * y[k];
-            }
-            X_T_y[i] = sum;
-        }
-    }
+    multiply_matrices(N_COLS, N_ROWS, 1, X_T, y, X_T_y);
 
     // LU decomposition
     u_11 = X_T_X[0][0];
     u_12 = X_T_X[0][1];
-    l_21 = X_T_X[1][0] / u_11;
+    if (u_11 == 0.0f) {
+        printf("Error: division by zero error (cannot solve)");
+        return 1;
+    }
+    else {
+        l_21 = X_T_X[1][0] / u_11;
+    }
     u_22 = X_T_X[1][1] - (l_21 * u_12);
 
     float L[N_COLS][N_COLS] = {{1, 0}, {l_21, 1}}, U[N_COLS][N_COLS] = {{u_11, u_12},{0, u_22}};
 
     // Solve Lz = y
-    z_0 = X_T_y[0];
-    z_1 = X_T_y[1] - L[1][0] * z_0;
-    float z[N_COLS] = {z_0, z_1};
+    z_0 = X_T_y[0][0];
+    z_1 = X_T_y[1][0] - L[1][0] * z_0;
+    float z[N_COLS][1] = {z_0, z_1};
 
     // Solve Ub = z
-    b_1 = z[1] / U[1][1];
-    b_0 = (z[0] - (U[0][1] * b_1)) / U[0][0];
-    float b[N_COLS] = {b_0, b_1};
+    b_1 = z[1][0] / U[1][1];
+    b_0 = (z[0][0] - (U[0][1] * b_1)) / U[0][0];
+    float b[N_COLS][1] = {b_0, b_1};
 
     // Calculate y_hat:
-    for (int i = 0; i < N_ROWS; i++) {
-        for (int j = 0; j < 1; j++) {
-            sum = 0.0f;
-            for (int k = 0; k < N_COLS; k++) {
-                sum += X[i][k] * b[k];
-            }
-            y_hat[i] = sum;
-        }
-    }
-
-    // for (int i = 0; i < N_COLS; i++) {
-    //     printf("%f %f\n", U[i][0], U[i][1]);
-    // }
+    multiply_matrices(N_ROWS, N_COLS, 1, X, b, y_hat);
 
     for (int i = 0; i < N_ROWS; i++) {
-        printf("%f \n", y_hat[i]);
+        printf("%f \n", y_hat[i][0]);
     }
-
-    // for (int j = 0; j < N_COLS; j++) {
-    //     for (int i = 0; i < N_ROWS; i++) {
-    //         printf("%f ", X_T[j][i]);
-    //     }
-    //     printf("\n");
-    // }
-
-
-
-    // for (int i = 0; i < N_ROWS; i++) {
-    //     printf("%f %f\n", X[i][0], X[i][1]);
-    // }
-    //
-    // for (int i = 0; i < n; i++) {
-    //     printf("%f\n", y[i]);
-    // }
 
     return 0;
 }
